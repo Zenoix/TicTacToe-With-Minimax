633f3faab3899a6f0cdc34022492115b9187b78a
Zenoix <41812358+Zenoix@users.noreply.github.com>
2022-01-05 02:30:41 +1300

    Continued working on player input and input validation

--- game.py
+++ game.py
@@ -1,23 +1,39 @@
-import random
+# import random
+
 
 class TicTacToe:
     def __init__(self):
-        self.board = [[" "] * 3] * 3
-        
+        self.board = [
+            [" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]
+        ]
+
     def print_board(self):
-        print("\n---+---+---\n".join(" " + " | ".join(row) for row in self.board) + " ")
-    
-    def make_player_move(self, player):
+        print("\n---+---+---\n".join(" " + " | ".join(row)
+                                     for row in self.board) + " ")
+
+    def make_player_move(self, player: int):
         symbol = "X" if player == 0 else "O"
+        prompt = 'Enter coordinate in the form "row col": '
         while True:
-            try:
-                x, y = map(int, input("Enter coordinate in the form row col: ").split())
-            except TypeError:
-                print("Invalid coordinate")
-            if check_valid_move(x, y):
+            coords = input(prompt)
+            if (coords := self.check_valid_move(coords)):
+                x, y = coords
                 self.board[x-1][y-1] = symbol
-    def check_valid_move(self, x, y):
-        return True
+                self.print_board()
+                break
+
+    def check_valid_move(self, coords: str):
+        coords = coords.split()
+        try:
+            coords = tuple(map(int, coords))
+            return coords if len(coords) == 2 else False
+        except ValueError:
+            return False
+
+
 tictactoe = TicTacToe()
 tictactoe.print_board()
-tictactoe.make_player_move()
+tictactoe.make_player_move(0)
+tictactoe.make_player_move(1)
